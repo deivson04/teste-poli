50b8dce3d55e948765e6458454898a1d39ff5e20
deivson <dayvson04@hotmail.com>
2022-05-06 03:02:14 -0300

    fazendo o formulario

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { Value } from 'sass';
+
+// import App from './App';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+  const handleSubmit = (e) =>{
+    e.prevenDefaul();
+    const formData = new FormData(e.target);
+   
+    const data = Object.fromEntries(formData);
+   
+   console.log('*** handleSubmit', data);
+  };
+  
+  return ( 
+  <form onSubmit={handleSubmit}>
+    <input type="text" name="email" placeholder="Email"  />
+    <button type="submit">Enviar</button>
+  </form>
+  
   );
 }
 
